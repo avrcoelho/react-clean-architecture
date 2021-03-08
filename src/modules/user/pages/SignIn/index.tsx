76015a6b218790425e7ea998df9bfff5e10da71d
avrcoelho <andrevrcoelho@hotmail.com>
2021-03-07 21:12:21 -0300

    style: main input

--- src/modules/user/pages/SignIn/index.tsx
+++ src/modules/user/pages/SignIn/index.tsx
@@ -1,5 +1,11 @@
+import InputText from '@/shared/components/inputs/Text';
+
 const SignIn = (): JSX.Element => {
-  return <div className="container px-4">Sing in</div>;
+  return (
+    <main className="bg-gray-800 p-4 w-full min-h-screen">
+      <InputText />
+    </main>
+  );
 };
 
 export default SignIn;
---
+++ src/shared/components/inputs/Text/index.tsx
@@ -0,0 +1,12 @@
+const InputText = (): JSX.Element => {
+  return (
+    <div>
+      <input
+        type="text"
+        className="w-full max-w-md h-10 bg-transparent border border-gray-600 rounded px-3 text-white"
+      />
+    </div>
+  );
+};
+
+export default InputText;

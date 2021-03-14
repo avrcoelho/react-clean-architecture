417f38e3f9de77498976a60779cc0f2fd56feea0
avrcoelho <andrevrcoelho@hotmail.com>
2021-03-14 14:52:45 -0300

    feat: add hook form on input

--- src/shared/components/inputs/Text/index.tsx
+++ src/shared/components/inputs/Text/index.tsx
@@ -1,10 +1,27 @@
-const InputText = (): JSX.Element => {
+import { InputHTMLAttributes, LegacyRef } from 'react';
+
+interface InputTextProps
+  extends InputHTMLAttributes<Omit<HTMLInputElement, 'ref'>> {
+  register: LegacyRef<HTMLInputElement>;
+  error?: string;
+}
+
+const InputText = ({
+  register,
+  error,
+  ...rest
+}: InputTextProps): JSX.Element => {
   return (
-    <div>
+    <div className="flex flex-col">
       <input
         type="text"
-        className="w-full max-w-md h-10 bg-transparent border border-gray-600 rounded px-3 text-white"
+        ref={register}
+        className={`w-full max-w-md h-10 bg-transparent border ${
+          error ? 'border-red-400' : 'border-gray-600'
+        }  rounded px-3 text-white`}
+        {...rest}
       />
+      {error && <small className="text-red-400">{error}</small>}
     </div>
   );
 };

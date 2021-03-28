c7c2f04bdd22912b139c2e9decd944b22ca8a39e
avrcoelho <andrevrcoelho@hotmail.com>
2021-03-27 21:21:25 -0300

    test: sign in hook

---
+++ src/modules/user/hooks/__tests__/useSignIn.spec.ts
@@ -0,0 +1,49 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import { toast } from 'react-toastify';
+
+import { left, right } from '@/shared/core/Either';
+import { useSignIn } from '../useSignIn';
+import SignInBuilder from '../../__tests__/builders/SignIn.builder';
+import ISignInDTO from '../../dtos/ISignIn.dto';
+import ISignInModel from '../../models/ISignIn.model';
+import { signInService } from '../../services';
+
+const mockHistoryPush = jest.fn();
+jest.mock('react-router-dom', () => ({
+  useHistory: () => ({
+    push: mockHistoryPush,
+  }),
+}));
+
+const spyOnSignInService = jest.spyOn(signInService, 'execute');
+
+describe('SignIn hook', () => {
+  it('should be able to have success login', async () => {
+    spyOnSignInService.mockImplementation(async () =>
+      right({} as ISignInModel),
+    );
+    const signInData: ISignInDTO = SignInBuilder.aSignInData().build();
+    const { result, waitForNextUpdate } = renderHook(() => useSignIn());
+
+    act(() => {
+      result.current.signIn(signInData);
+    });
+    await waitForNextUpdate();
+
+    expect(mockHistoryPush).toHaveBeenCalled();
+  });
+
+  it('should be able to have error login', async () => {
+    spyOnSignInService.mockImplementation(async () => left('has error'));
+    const spyToastError = jest.spyOn(toast, 'error');
+    const signInData: ISignInDTO = SignInBuilder.aSignInData().build();
+    const { result, waitForNextUpdate } = renderHook(() => useSignIn());
+
+    act(() => {
+      result.current.signIn(signInData);
+    });
+    await waitForNextUpdate();
+
+    expect(spyToastError).toHaveBeenCalled();
+  });
+});

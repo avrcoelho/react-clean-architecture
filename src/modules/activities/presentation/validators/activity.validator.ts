3467f2667340b6db631834ab7aad470e40f9c73c
avrcoelho <andrevrcoelho@hotmail.com>
2021-05-30 15:05:53 -0300

    refactor: validator

--- src/modules/activities/presentation/validators/activity.validator.ts
+++ src/modules/activities/presentation/validators/activity.validator.ts
@@ -3,8 +3,17 @@ import * as yup from 'yup';
 import InputMessages from '@/shared/presentation/constants/inputMessages';
 
 const activityValidator = yup.object({
-  time: yup.string().required(InputMessages.RequiredFiel),
-  date: yup.string().required(InputMessages.RequiredFiel),
+  time: yup
+    .string()
+    .matches(/^\d{2}:\d{2}$/, InputMessages.RequiredFiel)
+    .required(InputMessages.RequiredFiel),
+  date: yup
+    .string()
+    .matches(
+      /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[012])\/\d{4}$/,
+      InputMessages.RequiredFiel,
+    )
+    .required(InputMessages.RequiredFiel),
   type: yup.string().required(InputMessages.RequiredFiel),
 });
 

6dc2a7a1b12bd32101785a9803c69322e00734d8
avrcoelho <andrevrcoelho@hotmail.com>
2021-06-06 14:31:15 -0300

    test: listing activities

--- src/modules/activities/presentation/components/ListActivities/index.tsx
+++ src/modules/activities/presentation/components/ListActivities/index.tsx
@@ -1,11 +1,18 @@
 import IActivityModel from '@/modules/activities/domain/models/IActivity.model';
+import Item from './Item';
 
 interface ListActivitiesProps {
   activities: IActivityModel[];
 }
 
 const ListActivities = ({ activities }: ListActivitiesProps) => {
-  return <section className="w-full sm:max-w-2xl p-4" />;
+  return (
+    <section className="w-full sm:max-w-2xl p-4">
+      {activities.map(activity => (
+        <Item key={activity.id} activity={activity} />
+      ))}
+    </section>
+  );
 };
 
 export default ListActivities;
